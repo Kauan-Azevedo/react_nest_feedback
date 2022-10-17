0b069cf791c01731c5fc36f7cd068a58e9b6be8d
Kauan Azevedo <kauan.azevedo@fontespromotora.com.br>
2022-10-17 17:25:29 -0300

    feat: connect to api

--- frontend/src/pages/Feedbacks/index.tsx
+++ frontend/src/pages/Feedbacks/index.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { api } from "../../services/api";
 
 export function Feedbacks() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {}, []);
+
   return (
     <>
       <div>conteudo feedbacks</div>
