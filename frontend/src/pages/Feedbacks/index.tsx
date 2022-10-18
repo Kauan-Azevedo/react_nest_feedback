7a7002fa2a2061325bc6f495d7a65ddd260cd5a6
Kauan Azevedo <kauan.azevedo@fontespromotora.com.br>
2022-10-18 10:07:45 -0300

    feat: feedback list

--- frontend/src/pages/Feedbacks/index.tsx
+++ frontend/src/pages/Feedbacks/index.tsx
@@ -1,14 +1,40 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
+import style from "./feedback.module.css";
 
 export function Feedbacks() {
-  const [data, setData] = useState("");
+  const [feedback, setFeedback] = useState<Array<Object>>();
 
-  useEffect(() => {}, []);
+  const getFeed: () => Promise<void> = async () => {
+    const { data } = await api.get("/feedback");
+    const prod = data;
+    setFeedback(prod);
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
 
   return (
-    <>
-      <div>conteudo feedbacks</div>
-    </>
+    <section>
+      <h2 className={style["feed-header"]}>Lista de Feedbacks</h2>
+      <div className={style["container"]}>
+        {feedback ? (
+          feedback.map((feed: any) => (
+            <div className={style["feed-card"]} key={feed.id}>
+              <h2 className={style["feed-title"]}>{feed.title}</h2>
+              <p className={style["feed-body"]}>{feed.content}</p>
+              <div className={style["feed-footer"]}>
+                <p className={style["feed-autor"]}>
+                  <strong>Autor:</strong> {feed.name}
+                </p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <h3>Não foi possivel renderizar os feedbacks</h3>
+        )}
+      </div>
+    </section>
   );
 }
