60cfe61c43af4624d3ae557e1ad4ad19dfc0b009
Kauan-Azevedo <kauan.azevedo@fontespromotora.com.br>
2023-03-07 10:23:15 -0300

    fix: error message

--- frontend/src/pages/Feedbacks/index.tsx
+++ frontend/src/pages/Feedbacks/index.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
-import { api } from '../../services/api';
-import style from './feedback.module.css';
-import { Feedback } from '../../interfaces/feedback';
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import style from "./feedback.module.css";
+import { Feedback } from "../../interfaces/feedback";
 
 export function Feedbacks() {
     const [feedback, setFeedback] = useState<Feedback[]>();
 
     const getFeed: () => Promise<void> = async () => {
-        const { data } = await api.get('/feedback');
+        const { data } = await api.get("/feedback");
         const prod = data;
         setFeedback(prod);
     };
@@ -18,24 +18,24 @@ export function Feedbacks() {
 
     return (
         <section>
-            <h2 className={style['feed-header']}>Lista de Feedbacks</h2>
-            <div className={style['container']}>
+            <h2 className={style["feed-header"]}>Lista de Feedbacks</h2>
+            <div className={style["container"]}>
                 {feedback ? (
                     feedback.map((feed: Feedback) => (
-                        <div className={style['feed-card']} key={feed.id}>
-                            <h2 className={style['feed-title']}>
+                        <div className={style["feed-card"]} key={feed.id}>
+                            <h2 className={style["feed-title"]}>
                                 {feed.title}
                             </h2>
-                            <p className={style['feed-body']}>{feed.content}</p>
-                            <div className={style['feed-footer']}>
-                                <p className={style['feed-autor']}>
+                            <p className={style["feed-body"]}>{feed.content}</p>
+                            <div className={style["feed-footer"]}>
+                                <p className={style["feed-autor"]}>
                                     <strong>Autor:</strong> {feed.name}
                                 </p>
                             </div>
                         </div>
                     ))
                 ) : (
-                    <h3>Nenhum feedback.</h3>
+                    <h3>Ocorreu um erro ao buscar os feedbacks.</h3>
                 )}
             </div>
         </section>
